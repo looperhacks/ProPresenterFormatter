196057efb71680c6f5fd9a5b9acb27183d28768f
Peter Jeschke <looperhacks@gmail.com>
2017-09-11 20:47:12 +0200

    Projektdateien hinzufügen.

---
+++ ProPresenterFormatter/MainForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ProPresenterFormatter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchFileButton = new System.Windows.Forms.Button();
+            this.filePathText = new System.Windows.Forms.TextBox();
+            this.primitiveDisplay = new System.Windows.Forms.RichTextBox();
+            this.slideBackButton = new System.Windows.Forms.Button();
+            this.slideDescriptionBox = new System.Windows.Forms.TextBox();
+            this.slideForwardButton = new System.Windows.Forms.Button();
+            this.advancedDisplay = new System.Windows.Forms.Integration.ElementHost();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // searchFileButton
+            // 
+            this.searchFileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchFileButton.Location = new System.Drawing.Point(243, 11);
+            this.searchFileButton.Name = "searchFileButton";
+            this.searchFileButton.Size = new System.Drawing.Size(99, 23);
+            this.searchFileButton.TabIndex = 0;
+            this.searchFileButton.Text = "Durchsuchen";
+            this.searchFileButton.UseVisualStyleBackColor = true;
+            this.searchFileButton.Click += new System.EventHandler(this.OpenFile);
+            // 
+            // filePathText
+            // 
+            this.filePathText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filePathText.Location = new System.Drawing.Point(12, 11);
+            this.filePathText.Name = "filePathText";
+            this.filePathText.Size = new System.Drawing.Size(225, 20);
+            this.filePathText.TabIndex = 1;
+            // 
+            // primitiveDisplay
+            // 
+            this.primitiveDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.primitiveDisplay.BackColor = System.Drawing.Color.Black;
+            this.primitiveDisplay.Location = new System.Drawing.Point(12, 40);
+            this.primitiveDisplay.Name = "primitiveDisplay";
+            this.primitiveDisplay.Size = new System.Drawing.Size(200, 237);
+            this.primitiveDisplay.TabIndex = 2;
+            this.primitiveDisplay.Text = "";
+            // 
+            // slideBackButton
+            // 
+            this.slideBackButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.slideBackButton.Location = new System.Drawing.Point(12, 283);
+            this.slideBackButton.Name = "slideBackButton";
+            this.slideBackButton.Size = new System.Drawing.Size(30, 23);
+            this.slideBackButton.TabIndex = 3;
+            this.slideBackButton.Text = "<";
+            this.slideBackButton.UseVisualStyleBackColor = true;
+            this.slideBackButton.Click += new System.EventHandler(this.SlideBack);
+            // 
+            // slideDescriptionBox
+            // 
+            this.slideDescriptionBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.slideDescriptionBox.Location = new System.Drawing.Point(48, 286);
+            this.slideDescriptionBox.Name = "slideDescriptionBox";
+            this.slideDescriptionBox.ReadOnly = true;
+            this.slideDescriptionBox.Size = new System.Drawing.Size(339, 20);
+            this.slideDescriptionBox.TabIndex = 4;
+            // 
+            // slideForwardButton
+            // 
+            this.slideForwardButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.slideForwardButton.Location = new System.Drawing.Point(393, 283);
+            this.slideForwardButton.Name = "slideForwardButton";
+            this.slideForwardButton.Size = new System.Drawing.Size(30, 23);
+            this.slideForwardButton.TabIndex = 5;
+            this.slideForwardButton.Text = ">";
+            this.slideForwardButton.UseVisualStyleBackColor = true;
+            this.slideForwardButton.Click += new System.EventHandler(this.SlideForward);
+            // 
+            // advancedDisplay
+            // 
+            this.advancedDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.advancedDisplay.Location = new System.Drawing.Point(218, 40);
+            this.advancedDisplay.Name = "advancedDisplay";
+            this.advancedDisplay.Size = new System.Drawing.Size(200, 237);
+            this.advancedDisplay.TabIndex = 6;
+            this.advancedDisplay.Text = "elementHost1";
+            this.advancedDisplay.Child = null;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Location = new System.Drawing.Point(348, 11);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 7;
+            this.saveButton.Text = "Speichern";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 318);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.advancedDisplay);
+            this.Controls.Add(this.slideForwardButton);
+            this.Controls.Add(this.slideDescriptionBox);
+            this.Controls.Add(this.slideBackButton);
+            this.Controls.Add(this.primitiveDisplay);
+            this.Controls.Add(this.filePathText);
+            this.Controls.Add(this.searchFileButton);
+            this.Name = "MainForm";
+            this.Text = "ProPresenterFormatter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchFileButton;
+        private System.Windows.Forms.TextBox filePathText;
+        private System.Windows.Forms.RichTextBox primitiveDisplay;
+        private System.Windows.Forms.Button slideBackButton;
+        private System.Windows.Forms.TextBox slideDescriptionBox;
+        private System.Windows.Forms.Button slideForwardButton;
+        private System.Windows.Forms.Integration.ElementHost advancedDisplay;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
+
---
+++ ProPresenterFormatter/MainForm.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Markup;
+using System.Xml.Linq;
+
+namespace ProPresenterFormatter
+{
+    public partial class MainForm : Form
+    {
+        private List<XElement> _groups;
+        private int currentSlide = 0;
+        private XDocument _loadedFile;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _loadedFile = XDocument.Load(openFileDialog.FileName);
+                filePathText.Text = openFileDialog.FileName;
+                _groups = new List<XElement>(_loadedFile.Root
+                    .Descendants("array")
+                    .First(el => el.Attribute("rvXMLIvarName").Value=="groups")
+                    .Descendants("RVSlideGrouping")
+                    .SelectMany(group => group
+                        .Descendants("array")
+                        .Where(el => el.Attribute("rvXMLIvarName").Value=="slides")
+                        .SelectMany(el => el.Descendants("RVDisplaySlide"))));
+                LoadSlide();
+            }
+        }
+
+        private void LoadSlide()
+        {
+            if (_groups.Count <= currentSlide)
+                return;
+            var slide = _groups[currentSlide];
+            slideDescriptionBox.Text = $"Slide {currentSlide} [name: {slide.Attribute("label")}]";
+            var strings = slide.Descendants("array")
+                .Where(arr => arr.Attribute("rvXMLIvarName").Value == "displayElements")
+                .SelectMany(el => el.Descendants("RVTextElement"))
+                .SelectMany(el => el.Descendants("NSString"));
+            var primitiveText = strings.Where(str => str.Attribute("rvXMLIvarName").Value == "RTFData").Select(el => el.Value).FirstOrDefault();
+            if (primitiveText != null)
+            {
+                var primitiveString = Encoding.UTF8.GetString(Convert.FromBase64String(primitiveText));
+                primitiveDisplay.Rtf = primitiveString;
+            }
+            var flowDocument = ReadFlowDocumentFromXml(strings) ?? CreateFlowDocumentFromRtf(strings);
+            XElement stringElement = new XElement("NSString", Convert.ToBase64String(Encoding.UTF8.GetBytes(XamlWriter.Save(flowDocument))));
+            stringElement.SetAttributeValue("rvXMLIvarName", "WinFlowData");
+            strings.First().Parent.Add(stringElement);
+            advancedDisplay.Child = new FlowDocumentReader() { Document = flowDocument };
+        }
+
+        private FlowDocument CreateFlowDocumentFromRtf(IEnumerable<XElement> strings)
+        {
+            var plainText = primitiveDisplay.Text;
+            var flowDocument = new FlowDocument() { Background = System.Windows.Media.Brushes.Black };
+            int counter = 1;
+            foreach (var line in plainText.Split('\n'))
+            {
+                if (line.Trim() == "")
+                    continue;
+                Run run = new Run(line)
+                {
+                    FontFamily = new System.Windows.Media.FontFamily("Flama"),
+                    FontStyle = FontStyles.Normal,
+                    FontWeight = (counter % 2 == 0) ? FontWeights.Thin : FontWeights.Regular,
+                    FontStretch = (counter % 2 == 0) ? FontStretches.SemiCondensed : FontStretches.Condensed,
+                    FontSize = (counter % 2 == 0) ? 65 : 80,
+                    Foreground = System.Windows.Media.Brushes.White
+                };
+                flowDocument.Blocks.Add(new Paragraph(run) { TextAlignment = TextAlignment.Center } );
+                counter++;
+            }
+
+            return flowDocument;
+        }
+
+        private FlowDocument ReadFlowDocumentFromXml(IEnumerable<XElement> strings)
+        {
+            var complexText = strings.Where(str => str.Attribute("rvXMLIvarName").Value == "WinFlowData").Select(el => el.Value).FirstOrDefault();
+            if (complexText != null)
+            {
+                var complexString = Encoding.UTF8.GetString(Convert.FromBase64String(complexText));
+                return XamlReader.Parse(complexString) as FlowDocument;
+            }
+            return null;
+        }
+
+        private void SlideBack(object sender, EventArgs e)
+        {
+            if (currentSlide > 0)
+                currentSlide--;
+            LoadSlide();
+        }
+
+        private void SlideForward(object sender, EventArgs e)
+        {
+            currentSlide++;
+            LoadSlide();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            // walk through every slide to make sure that it's converted
+            
+            for (currentSlide = 0; currentSlide < _groups.Count; currentSlide++)
+            {
+                LoadSlide();
+            }
+            _loadedFile.Save(filePathText.Text);
+        }
+    }
+}
